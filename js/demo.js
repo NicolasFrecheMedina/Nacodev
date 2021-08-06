b984522322ffeec66a1c5831dec1ae39b94465b3
Nicolas Freche Medina <nicolas.freche@gmail.com>
2021-08-06 12:18:35 +0200

    Sketch effect on NacoCard, adding logo resized + effect, missing last card for block

--- js/demo.js
+++ js/demo.js
@@ -45,7 +45,6 @@
 //   document.getElementById("appear-conception").style.display = "block";
 // }
 
-
 // test style modal
 
 $(document).ready(function () {
@@ -54,44 +53,49 @@ $(document).ready(function () {
     $("#modal-container").removeAttr("class").addClass(buttonId);
     $("body").addClass("modal-active");
   });
-
   $('.close').click(function () {
     $("#modal-container").addClass("out");
     $("body").removeClass("modal-active");
   });
 
+  $('#service-creation').toggle(function() {
+        $('.appear-conception').css({opacity: 1});
+        $('.effect-conception').css({color: 'white'});
+        $('.effect-conception').css({background: 'linear-gradient(to right, orange, yellow)'});
+        $('#service-creation').css({background: 'linear-gradient(to right, orange, yellow)'});
+        $('#service-creation').css({color: 'white'});
+      }, function(){
+        $('.appear-conception').css({opacity: 0});
+        $('.effect-conception').css({color: 'black'});
+        $('.effect-conception').css({background: 'linear-gradient(135deg, #fff, #d2d2d2)'});
+         $('#service-creation').css({color: 'black'});
+        $('#service-creation').css({background: 'linear-gradient(135deg, #fff, #d2d2d2)'});
+      });
+
   $('#service-entrepreneur').toggle(function() {
       $('.appear-developpement').css({opacity: 1});
       $('.effect-developpement').css({color: 'white'});
+      $('.effect-developpement').css({background: 'linear-gradient(to right, red, orange)'});
       $('#service-entrepreneur').css({background: 'linear-gradient(to right, red, orange)'});
       $('#service-entrepreneur').css({color: 'white'});
     }, function(){
       $('.appear-developpement').css({opacity: 0});
       $('.effect-developpement').css({color: 'black'});
+      $('.effect-developpement').css({background: 'linear-gradient(135deg, #fff, #d2d2d2)'});
       $('#service-entrepreneur').css({color: 'black'});
       $('#service-entrepreneur').css({background: 'linear-gradient(135deg, #fff, #d2d2d2)'});
-    });
-
-    $('#service-creation').toggle(function() {
-      $('.appear-conception').css({opacity: 1});
-      $('.effect-conception').css({color: 'white'});
-      $('#service-creation').css({background: 'linear-gradient(to right, orange, yellow)'});
-      $('#service-creation').css({color: 'white'});
-    }, function(){
-      $('.appear-conception').css({opacity: 0});
-      $('.effect-conception').css({color: 'black'});
-      $('#service-creation').css({color: 'black'});
-      $('#service-creation').css({background: 'linear-gradient(135deg, #fff, #d2d2d2)'});
     });
 
     $('#service-refonte').toggle(function() {
       $('.appear-refonte').css({opacity: 1});
       $('.effect-refonte').css({color: 'white'});
+      $('.effect-refonte').css({background: 'linear-gradient(to right, #00C853, #B2FF59)'});
       $('#service-refonte').css({background: 'linear-gradient(to right, #00C853, #B2FF59)'});
       $('#service-refonte').css({color: 'white'});
     }, function(){
       $('.appear-refonte').css({opacity: 0});
       $('.effect-refonte').css({color: 'black'});
+      $('.effect-refonte').css({background: 'linear-gradient(135deg, #fff, #d2d2d2)'});
       $('#service-refonte').css({color: 'black'});
       $('#service-refonte').css({background: 'linear-gradient(135deg, #fff, #d2d2d2)'});
     });
@@ -99,14 +103,37 @@ $(document).ready(function () {
     $('#service-freelance').toggle(function() {
       $('.appear-freelance').css({opacity: 1});
       $('.effect-freelance').css({color: 'white'});
+      $('.effect-freelance').css({background: 'linear-gradient(to right, purple, pink)'});
       $('#service-freelance').css({background: 'linear-gradient(to right, purple, pink)'});
       $('#service-freelance').css({color: 'white'});
     }, function(){
       $('.appear-freelance').css({opacity: 0});
       $('.effect-freelance').css({color: 'black'});
+      $('.effect-freelance').css({background: 'linear-gradient(135deg, #fff, #d2d2d2)'});
       $('#service-freelance').css({color: 'black'});
       $('#service-freelance').css({background: 'linear-gradient(135deg, #fff, #d2d2d2)'});
     });
+
+  $('.btn-sketch').click(function(){
+    var btnSketch = $(this).attr('id');
+    $('#sketch-container').removeAttr('class').addClass(btnSketch);
+    $('body').addClass('sketch-active');
+  })
+  $('.close').click(function(){
+    $('#sketch-container').addClass('out');
+    $('body').removeClass('sketch-active');
+  });
+
+  $('.btn-sketch-n').click(function(){
+    var btnSketch = $(this).attr('id');
+    $('#sketch-container-n').removeAttr('class').addClass(btnSketch);
+    $('body').addClass('sketch-n-active');
+  })
+  $('.close').click(function(){
+    $('#sketch-container-n').addClass('out');
+    $('body').removeClass('sketch-n-active');
+  });
+
 });
 
 
