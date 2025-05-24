5ca68f4923ae6e343aed4f808fc89282e4ef5c56
Naco Dev <73950133+NicolasFrecheMedina@users.noreply.github.com>
2025-05-24 18:26:08 +0200

    sign + rework text

--- js/demo.js
+++ js/demo.js
@@ -1,3 +1,18 @@
+/* <!--
+b.             8          .8.              ,o888888o.        ,o888888o.               8 888888888o.      8 8888888888   `8.`888b           ,8'
+888o.          8         .888.            8888     `88.   . 8888     `88.             8 8888    `^888.   8 8888          `8.`888b         ,8'
+Y88888o.       8        :88888.        ,8 8888       `8. ,8 8888       `8b            8 8888        `88. 8 8888           `8.`888b       ,8'
+.`Y888888o.    8       . `88888.       88 8888           88 8888        `8b           8 8888         `88 8 8888            `8.`888b     ,8'
+8o. `Y888888o. 8      .8. `88888.      88 8888           88 8888         88           8 8888          88 8 888888888888     `8.`888b   ,8'
+8`Y8o. `Y88888o8     .8`8. `88888.     88 8888           88 8888         88           8 8888          88 8 8888              `8.`888b ,8'
+8   `Y8o. `Y8888    .8' `8. `88888.    88 8888           88 8888        ,8P           8 8888         ,88 8 8888               `8.`888b8'
+8      `Y8o. `Y8   .8'   `8. `88888.   `8 8888       .8' `8 8888       ,8P            8 8888        ,88' 8 8888                `8.`888'
+8         `Y8o.`  .888888888. `88888.     8888     ,88'   ` 8888     ,88'             8 8888    ,o88P'   8 8888                 `8.`8'
+8            `Yo .8'       `8. `88888.     `8888888P'        `8888888P'               8 888888888P'      8 888888888888          `8.`
+
+                                                                Powered by 🅽🅰🅲🅾 🅳🅴🆅 ✨
+--> */
+
 
 $(document).ready(function () {
   $(".button").click(function () {
@@ -84,6 +99,78 @@ $(document).ready(function () {
       $('#service-freelance').css({background: 'linear-gradient(135deg, #fff, #d2d2d2)'});
     });
 
+    $('.enter-accordion').click(function() {
+      $(this).next().slideToggle('500').css('');
+    });
+    $('.enter-accordion2').click(function() {
+      $(this).next().slideToggle('500').css('');
+    });
+    $('.C').toggle(function() {
+      $('.left-barC').css({transform: 'rotate(-45deg)'});
+      $('.right-barC').css({transform: 'rotate(45deg)'});
+    }, function(){
+      $('.left-barC').css({transform: 'rotate(45deg)'});
+      $('.right-barC').css({transform: 'rotate(-45deg)'});
+    });
+
+    $('.D').toggle(function() {
+      $('.left-barD').css({transform: 'rotate(-45deg)'});
+      $('.right-barD').css({transform: 'rotate(45deg)'});
+    }, function(){
+      $('.left-barD').css({transform: 'rotate(45deg)'});
+      $('.right-barD').css({transform: 'rotate(-45deg)'});
+    });
+
+    $('.R').toggle(function() {
+      $('.left-barR').css({transform: 'rotate(-45deg)'});
+      $('.right-barR').css({transform: 'rotate(45deg)'});
+    }, function(){
+      $('.left-barR').css({transform: 'rotate(45deg)'});
+      $('.right-barR').css({transform: 'rotate(-45deg)'});
+    });
+
+    $('.P').toggle(function() {
+      $('.left-barP').css({transform: 'rotate(-45deg)'});
+      $('.right-barP').css({transform: 'rotate(45deg)'});
+    }, function(){
+      $('.left-barP').css({transform: 'rotate(45deg)'});
+      $('.right-barP').css({transform: 'rotate(-45deg)'});
+    });
+
+    $('.C2').toggle(function() {
+      $('.left-barC2').css({transform: 'rotate(45deg)'});
+      $('.right-barC2').css({transform: 'rotate(-45deg)'});
+    }, function(){
+      $('.left-barC2').css({transform: 'rotate(-45deg)'});
+      $('.right-barC2').css({transform: 'rotate(45deg)'});
+    });
+
+    $('.D2').toggle(function() {
+      $('.left-barD2').css({transform: 'rotate(45deg)'});
+      $('.right-barD2').css({transform: 'rotate(-45deg)'});
+    }, function(){
+      $('.left-barD2').css({transform: 'rotate(-45deg)'});
+      $('.right-barD2').css({transform: 'rotate(45deg)'});
+    });
+
+    $('.R2').toggle(function() {
+      $('.left-barR2').css({transform: 'rotate(45deg)'});
+      $('.right-barR2').css({transform: 'rotate(-45deg)'});
+    }, function(){
+      $('.left-barR2').css({transform: 'rotate(-45deg)'});
+      $('.right-barR2').css({transform: 'rotate(45deg)'});
+    });
+
+    $('.P2').toggle(function() {
+      $('.left-barP2').css({transform: 'rotate(45deg)'});
+      $('.right-barP2').css({transform: 'rotate(-45deg)'});
+    }, function(){
+      $('.left-barP2').css({transform: 'rotate(-45deg)'});
+      $('.right-barP2').css({transform: 'rotate(45deg)'});
+    });
+
+    
+
   $('.btn-sketch').click(function(){
     var btnSketch = $(this).attr('id');
     $('#sketch-container').removeAttr('class').addClass(btnSketch);
@@ -122,4 +209,150 @@ function changeText(text) {
 }
 
 
+let activeSlide = false;
 
+document.querySelector('.scroller').addEventListener('mousedown',function(){
+  activeSlide = true;
+  document.querySelector('.scroller').classList.add('scrolling');
+});
+document.body.addEventListener('mouseup',function(){
+  activeSlide = false;
+  document.querySelector('.scroller').classList.remove('scrolling');
+});
+document.body.addEventListener('mouseleave',function(){
+  activeSlide = false;
+  document.querySelector('.scroller').classList.remove('scrolling');
+});
+document.body.addEventListener('mousemove',function(e){
+  if (!activeSlide) return;
+  let x = e.pageX;
+  x -= document.querySelector('.wrapper-slider').getBoundingClientRect().left;
+  scrollIt(x);
+});
+function scrollIt(x){
+    let transform = Math.max(0,(Math.min(x,document.querySelector('.wrapper-slider').offsetWidth)));
+    document.querySelector('.after').style.width = transform+"px";
+    document.querySelector('.scroller').style.left = transform-25+"px";
+}
+
+
+// // And finally let's repeat the process for touch events
+// // first our middle scroller...
+// document.querySelector('.scroller').addEventListener('touchstart',function(){
+//   activeSlide = true;
+//   document.querySelector('.scroller').classList.add('scrolling');
+// });
+// document.body.addEventListener('touchend',function(){
+//   activeSlide = false;
+//   document.querySelector('.scroller').classList.remove('scrolling');
+// });
+// document.body.addEventListener('touchcancel',function(){
+//   activeSlide = false;
+//   document.querySelector('.scroller').classList.remove('scrolling');
+// });
+
+
+
+document.querySelector('.scroller').addEventListener('touchstart',function(){
+  activeSlide = true;
+  document.querySelector('.scroller').classList.add('scrolling');
+});
+document.body.addEventListener('touchend',function(){
+  activeSlide = false;
+  document.querySelector('.scroller').classList.remove('scrolling');
+});
+// document.body.addEventListener('touchcancel',function(){
+//   activeSlide = false;
+//   document.querySelector('.scroller').classList.remove('scrolling');
+// });
+document.body.addEventListener('touchmove',function(e){
+  if (!activeSlide) return;
+  let x = e.pageX;
+  x -= document.querySelector('.wrapper-slider').getBoundingClientRect().left;
+  scrollIt(x);
+});
+function scrollIt(x){
+    let transform = Math.max(0,(Math.min(x,document.querySelector('.wrapper-slider').offsetWidth)));
+    document.querySelector('.after').style.width = transform+"px";
+    document.querySelector('.scroller').style.left = transform-25+"px";
+}
+
+scrollIt();
+
+
+
+// // Let's use the 'active' variable to let us know when we're using it
+// let activeResponsive = false;
+
+// // First we'll have to set up our event listeners
+// // We want to watch for clicks on our scroller
+// document.querySelector('.iphone-scroller').addEventListener('mousedown',function(){
+//   activeResponsive = true;
+//   // Add our scrolling class so the scroller has full opacity while activeResponsive
+//   document.querySelector('.iphone-scroller').classList.add('iphone-scrolling');
+// });
+// // We also want to watch the body for changes to the state,
+// // like moving around and releasing the click
+// // so let's set up our event listeners
+// document.body.addEventListener('mouseup',function(){
+//   activeResponsive = false;
+//   document.querySelector('.iphone-scroller').classList.remove('iphone-scrolling');
+// });
+// document.body.addEventListener('mouseleave',function(){
+//   activeResponsive = false;
+//   document.querySelector('.iphone-scroller').classList.remove('iphone-scrolling');
+// });
+
+// // Let's figure out where their mouse is at
+// document.body.addEventListener('mousemove',function(e){
+//   if (!activeResponsive) return;
+//   // Their mouse is here...
+//   let y = e.pageX;
+//   // but we want it relative to our wrapper
+//   y -= document.querySelector('.iphone-wrapper-slider').getBoundingClientRect().left;
+//   // Okay let's change our state
+//   scrollIt(y);
+// });
+
+// // Let's use this function
+// function scrollIt(y){
+//     let transform = Math.max(0,(Math.min(y,document.querySelector('.iphone-wrapper-slider').offsetWidth)));
+//     document.querySelector('.iphone-after').style.width = transform+"px";
+//     document.querySelector('.iphone-scroller').style.left = transform-25+"px";
+// }
+
+// // Let's set our opening state based off the width, 
+// // we want to show a bit of both images so the user can see what's going on
+// // scrollIt();
+
+// // And finally let's repeat the process for touch events
+// // first our middle scroller...
+// document.querySelector('.iphone-scroller').addEventListener('touchstart',function(){
+//   activeResponsive = true;
+//   document.querySelector('.iphone-scroller').classList.add('iphone-scrolling');
+// });
+// document.body.addEventListener('touchend',function(){
+//   activeResponsive = false;
+//   document.querySelector('.iphone-scroller').classList.remove('iphone-scrolling');
+// });
+// document.body.addEventListener('touchcancel',function(){
+//   activeResponsive = false;
+//   document.querySelector('.iphone-scroller').classList.remove('iphone-scrolling');
+// });
+
+
+// function cb(response) {
+//   document.getElementById('visits').innerText = response.value;
+// }
+
+
+// function clicked() {
+//   var xhr = new XMLHttpRequest();
+//   xhr.open("GET", "https://api.countapi.xyz/hit/nacodev.com/awesomeclick");
+//   xhr.responseType = "json";
+//   xhr.onload = function() {
+//     document.getElementById('visits2').innerText = this.response.value;
+//       // alert(`This button has been clicked ${this.response.value} times!`);
+//   }
+//   xhr.send();
+// }
